116d0a3a177467ae404b944c3e8623263bb3a6dc
Jimmy Liao <gooyumi@gmail.com>
2021-01-05 21:17:29 +0800

    Change to dark theme and add key for table

--- src/COVID19/COVID19.js
+++ src/COVID19/COVID19.js
@@ -1,67 +1,78 @@
 import useSWR from 'swr';
 
 import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 
+import {
+  Grid,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from '@material-ui/core';
 import { fetcher, config } from '../util/swrSettings';
 
 const useStyles = makeStyles({
+  tableContainer: {
+    padding: 12,
+  },
   table: {
     minWidth: 650,
   },
 });
 
-const COUNT = 10;
+const COUNT = 8;
 
 const COVID19 = () => {
   const url = 'https://storage.googleapis.com/covid19-open-data/v2/TW/main.json';
   const { data: { data } } = useSWR(url, fetcher, config);
   const { length } = data;
   const res = data.slice(length - COUNT);
+  const country = res[0][5];
 
   const classes = useStyles();
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Country</TableCell>
-            <TableCell>Date</TableCell>
-            <TableCell>Total Confirmed</TableCell>
-            <TableCell>Total Deceased</TableCell>
-            <TableCell>Total Recovered</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {res.map((item) => (
-            <TableRow key="key">
-              <TableCell component="th" scope="row">
-                {item[5]}
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {item[1]}
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {item[19]}
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {item[20]}
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {item[21]}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Grid container justify="center">
+      <Grid item xs={9}>
+        <TableContainer className={classes.tableContainer} component={Paper}>
+          <Typography variant="h3" component="h3" gutterBottom>
+            {country}
+          </Typography>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Date</TableCell>
+                <TableCell>Total Confirmed</TableCell>
+                <TableCell>Total Deceased</TableCell>
+                <TableCell>Total Recovered</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {res.map((item) => (
+                <TableRow key={item[1]}>
+                  <TableCell component="th" scope="row">
+                    {item[1]}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {item[19]}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {item[20]}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {item[21]}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 // eslint-disable-next-line camelcase
 import { unstable_createRoot } from 'react-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
 const root = document.getElementById('root');
-unstable_createRoot(root).render(<App />);
+unstable_createRoot(root).render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
